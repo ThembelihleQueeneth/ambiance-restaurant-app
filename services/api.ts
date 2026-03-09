9fbb5e832b944e7c6bc57b78b90d76a619ccb4db
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2026-03-09 12:07:46 +0200

    Modifying service api

--- services/api.ts
+++ services/api.ts
@@ -1,13 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-// Replace with your local IP if testing on a physical device
-const BASE_URL = 'http://192.168.1.111:5000';
+const BASE_URL = "http://10.192.0.139:5000";
 
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
